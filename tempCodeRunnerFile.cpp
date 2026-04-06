ad29eaede0bbfa7372b38c63a2dbeb0a9dc495d1
FazaViranza <201728547+FazaViranza@users.noreply.github.com>
2026-04-06 17:47:20 +0700

    Membuat struct orang

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+struct Orang
+{
+    string nama;
+    string alamat;
+    int umur;
+};
+
+int main() {
+    Orang mhs;
+  
+    cout << "Mengisi Data" << endl;
+
+    cout << "Nama: ";
+    cin >> mhs.nama;
+    cout << "Alamat: ";
+    cin >> mhs.alamat;
+    cout << "Umur: ";
+    cin >> mhs.umur;
+    cout << endl;
+
+    //tamplkan
+    cout << "Tampil Data" << endl;
+    cout << "Nama: " << mhs.nama << endl;
+    cout << "Alamat: " << mhs.alamat << endl;
+    cout << "Umur: " << mhs.umur << endl;
+}
