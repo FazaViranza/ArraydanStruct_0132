097cbc76c56a871c6ab558d840905f4dbc2c6f95
FazaViranza <201728547+FazaViranza@users.noreply.github.com>
2026-04-06 18:51:03 +0700

    Menambahkan array supaya bisa menginput lebih dari satu data

---
+++ 5.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+struct detailAlamat
+{
+    string desa;
+    string kota;
+};
+
+
+struct Orang
+{
+    string nama;
+    detailAlamat alamat;
+    int umur;
+};
+
+int main() {
+    Orang mhs[3];
+    //mengisi data
+
+    for(int i = 0; i < 3; i++) {
+        cout << "Data Mahasiswa ke-" << i+1 << endl;
+        cout << "Nama: ";
+        getline(cin, mhs[i].nama);
+        cout << "Desa: ";
+        getline(cin, mhs[i].alamat.desa);
+        cout << "Kota: ";
+        getline(cin, mhs[i].alamat.kota);
+        cout << "Umur: ";
+        cin >> mhs[i].umur;
+        cout << endl;
+    }
+
+    // menampilkan data
+    cout << "Tampil Data" << endl;
+    for(int i = 0; i < 3; i++) {
+        cout << "Data Mahasiswa ke-" << i+1 << endl;
+        cout << "Nama: " << mhs[i].nama << endl;
+        cout << "Desa: " << mhs[i].alamat.desa << endl;
+        cout << "Kota: " << mhs[i].alamat.kota << endl;
+        cout << "Umur: " << mhs[i].umur << endl;
+    }
+}
