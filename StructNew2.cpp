a706b41370f64552bd1792f6084fbe07c2101e6d
FazaViranza <201728547+FazaViranza@users.noreply.github.com>
2026-04-06 18:30:21 +0700

    Menginput data

--- StructNew2.cpp
+++ StructNew2.cpp
@@ -15,3 +15,19 @@ struct Orang
     int umur;
 };
 
+int main() {
+    Orang mhs;
+    //mengisi data
+    cout << "Mengisi Data" << endl;
+
+    cout << "Nama: ";
+    cin >> mhs.nama;
+    cout << "Desa: ";
+    cin >> mhs.alamat.desa;
+    cout << "Kota: ";
+    cin >> mhs.alamat.kota;
+    cout << "Umur: ";
+    cin >> mhs.umur;
+    cout << endl;
+
+}
